3309de9fd8af8255968a709e976ba6136c62ed6d
Ignacio Contreras <ignacio@nachovia.com>
2013-03-03 14:49:08 +0100

    First real commit

---
+++ anser/api.py
@@ -0,0 +1,31 @@
+import socket
+
+
+class Anser(object):
+    def __init__(self, name):
+        self.name = name
+
+
+    def run(self, ip='127.0.0.1', port='8053', buffer_size=1024):
+        self.ip = ip
+        self.port = port
+        self.buffer_size = buffer_size
+        self.socket = Anser._get_UDP_socket()
+        self.socket.bind((self.ip, self.port))
+        self._listen()
+            
+
+    def _listen(self):
+        while True:
+            data, addr = self.socket.recvfrom(self.buffer_size)
+            print "received data: {}".format(data)
+
+
+    @staticmethod
+    def _get_UDP_socket():
+        return socket.socket(
+                socket.AF_INET,
+                socket.SOCK_DGRAM
+                )
+
+     
