a7a7034f27fbf1b1564457aa5aa02e858a489db5
Ignacio Contreras <ignacio@nachovia.com>
2013-03-03 14:57:22 +0100

    Add debug option

--- anser/api.py
+++ anser/api.py
@@ -6,9 +6,11 @@ class Anser(object):
         self.name = name
 
 
-    def run(self, ip='127.0.0.1', port='8053', buffer_size=1024):
+    def run(self, ip='127.0.0.1', port='8053',
+            buffer_size=1024, debug=False):
         self.ip = ip
         self.port = port
+        self.debug = debug
         self.buffer_size = buffer_size
         self.socket = Anser._get_UDP_socket()
         self.socket.bind((self.ip, self.port))
@@ -16,6 +18,8 @@ class Anser(object):
             
 
     def _listen(self):
+        if self.debug:
+            print "Listening at {}:{}".format(self.ip, self.port)
         while True:
             data, addr = self.socket.recvfrom(self.buffer_size)
             print "received data: {}".format(data)
