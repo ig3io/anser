daaca07d5a446f25bd917fc08db05ccd2ddd8acf
Ignacio Contreras <ignacio@nachovia.com>
2013-03-05 17:51:32 +0100

    Update... almost everything. Change of project focus. UDP server with BSON messaging

--- anser/server.py
+++ anser/server.py
@@ -1,84 +1,69 @@
 import socket
 
 
-def tcp_socket():
-    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-def upd_socket():
-    return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-
 class Anser(object):
-    def __init__(self, name, protocol='udp'):
-        if protocol == 'tcp':
-            return TCPServer(name)
-        elif protocol == 'udp':
-            return UDPServer(name)
-        else:
-            raise ValueError("{} protocol not supported".format(protocol))
+    """
+    A class that models an Anser app. A configurable UDP server with
+    BSON messaging.
 
+    While an Anser object is running on a specific port, each incoming
+    UDP message may trigger one or more actions, according to specified
+    criteria.
 
-def default_action(data, address):
-    print "{} - {}".format(address, data)
+    """ 
+    def __init__(self, name, debug=False):
+        """
+        Initializes just the basic stuff. Main data structures are
+        initiliazed inside ``run`` method
 
-class BaseServer(object):
-    def __init__(self, name):
+        """
         self.name = name
+        self.actions = []
+        self.debug = debug
 
-    def run(self, ip, port, buffer_size, debug, action):
+    def run(self, ip='127.0.0.1', port=8080,
+            buffer_size=1024):
         self.ip = ip
         self.port = port
         self.buffer_size = buffer_size
-        self.debug = debug
-        if not action is None:
-            self.action = default_action
-        else:
-            self.action = action
-
-
-class UDPServer(BaseServer):
-    def __init__(self, name):
-        super(UDPServer, self).__init__()
-
-    def run(self, ip='127.0.0.1', port=8080,
-            buffer_size=1024, debug=False,
-            action=None):
-        super(UDPServer, self).run(ip, port, buffer_size,
-                                   debug, action)
-        self.socket = udp_socket()
+        self.socket = Anser._get_udp_socket()
         self.socket.bind((self.ip, self.port))
         self._listen()
 
-    def _listen():
+
+    def _process(self, data, address):
+        for action in self.actions:
+            action(data, address)
+
+
+    def _listen(self):
         if self.debug:
-            print "UDP server listening at {}.{}".format(
-                        self.socket_type.upper(),
+            print "Server listening at {0}.{1}".format(
                         self.ip,
                         self.port
                     )
         while True:
-            data, addr = self.socket.recvfrom(self.buffer_size)
-            self.action(data, addr)
+            data, address = self.socket.recvfrom(self.buffer_size)
+            if self.debug:
+                print "Message received:"
+                print "Address: {0}".format(address)
+                print "Data: " + data
+            self._process(data, address)
 
 
-class TCPServer(BaseServer):
-    def __init__(self, anem):
-        super(TCPServer, self).__init__()
+    def add_action(self, action, category):
+        self.actions.append(action)
+        if self.debug:
+            print "{0} action added".format(action.__name__)
 
-    
-    def run(self, ip='127.0.0.1', port=8080,
-            buffer_size=1024, debug=False,
-            action=None):
-        super(TCPServer, self).run(ip, port, buffer_size,
-                                   debug, action)
-        self.socket = tcp_socket()
-        self.socket.bind((self.ip, self.port))
-        self._listen()
 
-    def _listen():
-        self.socket.listen(1)
-        self.conn, self.addr = self.socket.accept()
-        while True:
-            data = self.conn.recv(self.buffer_size)
-            self.action(data, self.addr)
+    def action(self, category):
+        def decorator(f):
+            self.add_action(f, category)
+            return f
+        return decorator
+
+
+    @staticmethod
+    def _get_udp_socket():
+        return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
