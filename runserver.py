edfa352981bcd15cb805ede98c30a20a77ff2fd6
Ignacio Contreras <ignacio@nachovia.com>
2013-03-05 17:51:32 +0100

    Updates... almost everything. Change of project focus. UDP server with BSON messaging

--- runserver.py
+++ runserver.py
@@ -1,7 +1,23 @@
 from anser import Anser
 
 
-server = Anser(__name__, protocol='udp')
-server.run(ip='127.0.0.1',
-           port=8083,
-           debug=True)
+server = Anser(__name__, debug=True)
+
+counter = 0
+
+def increase_count():
+    global counter
+    counter += 1
+    return counter
+
+@server.action('default')
+def action_a(data, address):
+    print "Count: " + str(increase_count())
+
+# Message filtering not implemented, yet
+@server.action('other_category')
+def action_b(data, address):
+    print ":D"
+
+server.run()
+
