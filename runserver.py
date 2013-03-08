c0fb7c52a20366dcb268bb5ea714ee37167ff9da
Ignacio Contreras <ignacio@nachovia.com>
2013-03-08 10:47:59 +0100

    Simplify runserver.py

--- runserver.py
+++ runserver.py
@@ -3,21 +3,12 @@ from anser import Anser
 
 server = Anser(__name__, debug=True)
 
-counter = 0
-
-def increase_count():
-    global counter
-    counter += 1
-    return counter
 
 @server.action('default')
-def action_a(data, address):
-    print "Count: " + str(increase_count())
+def action_a(message, address):
+    print message['type']
+    print message['body']
 
-# Message filtering not implemented, yet
-@server.action('other_category')
-def action_b(data, address):
-    print ":D"
 
 server.run()
 
