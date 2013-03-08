e606a833585fc05cd50ef323db8e49fa90228fbb
Ignacio Contreras <ignacio@nachovia.com>
2013-03-08 10:48:17 +0100

    Adds a oversimplified demo client

---
+++ client.py
@@ -0,0 +1,17 @@
+import bson
+import socket
+
+
+def main():
+    udp_ip = '127.0.0.1'
+    udp_port = 8080
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    message = bson.dumps({
+        'body': 'Hello there!',
+        'type': 'default'
+        })
+    sock.sendto(message, (udp_ip, udp_port))
+
+
+if __name__ == '__main__':
+    main()
