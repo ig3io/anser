a194250d3e0c9646c284a36ca39ab6f86acbdaf5
Ignacio Contreras <ignacio@nachovia.com>
2013-03-08 13:03:27 +0100

    Renames all.py to basic.py. Minor additions to the unit testing skeleton

---
+++ test/basic.py
@@ -0,0 +1,21 @@
+import unittest
+from anser import Anser
+
+
+class BasicTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_creation(self):
+        pass
+
+    def test_receive(self):
+        pass
+
+    def test_send(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
