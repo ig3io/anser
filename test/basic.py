6962ceed3a2b0dceb086dab375cbf498ea115f68
Ignacio Contreras <ignacio@nachovia.com>
2013-03-08 13:11:35 +0100

    Adds first tests

--- test/basic.py
+++ test/basic.py
@@ -7,11 +7,35 @@ class BasicTest(unittest.TestCase):
     def setUp(self):
         pass
 
+
     def test_creation(self):
-        pass
+        server = Anser(__file__)
+        self.assertEquals(server.name, __file__)
+
+
+    def test_creation_explicit_no_debug(self):
+        server = Anser(__file__, debug=False)
+        self.assertFalse(server.debug)
+
+
+    def test_creation_implicit_no_debug(self):
+        server = Anser(__file__)
+        self.assertFalse(server.debug)
+
+    def test_creation_explicit_debug(self):
+        server = Anser(__file__, debug=True)
+        self.assertTrue(server.debug)
+
+    def test_add_action(self):
+        server = Anser(__file__)
+        @server.action('default')
+        def dummy_action(message, address):
+            pass
+        self.assertTrue(dummy_action in server.actions)
 
     def test_receive(self):
         pass
+
 
     def test_send(self):
         pass
