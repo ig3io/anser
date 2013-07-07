6b2f42d9d1d979c2ef7cb17a3c79d271e6b1ea4b
ignaciocontreras <ignacio@nachovia.com>
2013-07-07 23:55:23 +0200

    Update examples

--- examples/server.py
+++ examples/server.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from anser import Anser
 
 
 server = Anser(__name__, debug=True)
 
-
 @server.action('default')
 def action_a(message, address):
     print("{0} - {1}".format(address, message))
-
 
 server.run()
