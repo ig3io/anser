0282c7eaecb32b736592c84cda1f7520c130c676
ignaciocontreras <ignacio@nachovia.com>
2013-07-08 18:53:17 +0200

    Update basic tests

--- test/basic.py
+++ test/basic.py
@@ -1,32 +1,24 @@
 import unittest
-from anser import Anser
+from anser import Anser, Client
 
 
-class BasicTest(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
+class BasicAnserTest(unittest.TestCase):
 
     def test_creation(self):
         server = Anser(__file__)
         self.assertEquals(server.name, __file__)
 
-
     def test_creation_explicit_no_debug(self):
         server = Anser(__file__, debug=False)
         self.assertFalse(server.debug)
-
 
     def test_creation_implicit_no_debug(self):
         server = Anser(__file__)
         self.assertFalse(server.debug)
 
-
     def test_creation_explicit_debug(self):
         server = Anser(__file__, debug=True)
         self.assertTrue(server.debug)
-
 
     def test_add_action(self):
         server = Anser(__file__)
@@ -36,12 +28,20 @@ class BasicTest(unittest.TestCase):
         self.assertTrue(dummy_action in server.actions)
 
 
-    def test_receive(self):
-        pass
+class BasicClientTest(unittest.TestCase):
 
+    def test_creation(self):
+        client = Client('10.0.0.1', 4000)
+        self.assertEquals(client.address, '10.0.0.1')
+        self.assertEquals(client.port, 4000)
 
-    def test_send(self):
-        pass
+    def test_creation_implicit_no_debug(self):
+        client = Client('10.0.0.1', 4000)
+        self.assertFalse(client.debug)
+
+    def test_creation_explicit_debug(self):
+        client = Client('10.0.0.1', 4000, debug=True)
+        self.assertTrue(client.debug)
 
 
 if __name__ == '__main__':
